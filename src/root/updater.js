401ae5d160b61d76a520f9fd11ba8d3fce156114
Tomas Weiss <tomasw@salsitasoft.com>
2016-06-09 16:46:31 +0200

    fixing routing issues

--- src/root/updater.js
+++ src/root/updater.js
@@ -1,16 +1,12 @@
 import { Updater, Matchers } from 'redux-elm';
-import { routerReducer } from 'react-router-redux';
 import counterUpdater, { init as counterInit } from '../counter/updater';
 
 export const initialModel = {
-  routing: routerReducer(),
-  root: {
-    counter1: counterInit('Counter 1'),
-    counter2: counterInit('Counter 2'),
-  }
+  counter1: counterInit('Counter 1'),
+  counter2: counterInit('Counter 2')
 };
 
 export default new Updater(initialModel)
-  .case('Counter1', (model, action) => ({ ...model, root: { ...model.root, counter1: counterUpdater(model.root.counter1, action) } }))
-  .case('Counter2', (model, action) => ({ ...model, root: { ...model.root, counter2: counterUpdater(model.root.counter2, action) } }))
+  .case('Counter1', (model, action) => ({ ...model, counter1: counterUpdater(model.counter1, action) }))
+  .case('Counter2', (model, action) => ({ ...model, counter2: counterUpdater(model.counter2, action) }))
   .toReducer();
