50e471c98a4c28a25f3cfcdb58db508995573d49
Jan Grzegorowski <grzegorowski@gmail.com>
2016-07-11 16:28:35 +0200

    feat: implement Template as stateless component

--- src/template.js
+++ src/template.js
@@ -1,24 +1,24 @@
 /**
- * Main template component. It is the place for defining general page structure.
+ * Main template component. It is the place for defining general page structure like navbars etc.
  */
 import React from 'react';
 import { Link } from 'react-router'; // just for navigation
 
-// TODO: maybe there is some better implementation 
-// e.g. using stateless functional components
-export default class Template extends React.Component {
-  render() {
-    return (
-      <div>
-        <nav>
-          <Link to="/one">counter1</Link>
-          <br/>
-          <Link to="/two">counter2</Link>
-        </nav>
-        <main>
-          {this.props.children}
-        </main>
-      </div>
-    )
-  }
-}
+const Template = (props) => (
+  <div>
+    <nav>
+      <Link to="/one">counter1</Link>
+      <br/>
+      <Link to="/two">counter2</Link>
+    </nav>
+    <main>
+      {props.children}
+    </main>
+  </div>
+);
+
+Template.propTypes = {
+  children: React.PropTypes.object,
+};
+
+export default Template;
